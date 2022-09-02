dd686b4fefe0160f78639c7cd71bd0e06e6ca293
Kilimanjaro874 <vantan2022.228995@gmail.com>
2022-09-02 15:42:50 +0900

    ヘリの位置PID制御まで追加(姿勢制御は未実装)

--- Assets/EnemyMove.cs
+++ Assets/EnemyMove.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D _rb2D;              // ヘリのリジッドボディ2D取得
+    [SerializeField]
+    private Transform _heliTargetPos;       // ヘリの位置PID制御目標位置
+    // 位置制御用パラメータ
+    Vector3 _preErrorPos = new Vector3(0, 0, 0);
+    Vector3 _integralError = new Vector3(0, 0, 0);
+    [SerializeField]
+    float _kp = 16f;        // P制御係数  
+    [SerializeField]
+    float _ki = 0.01f;      // I制御係数
+    [SerializeField]
+    float _kd = 3.5f;       // D制御係数
+    float _velMax = 1.0f;   // 速度上限
+    float _accMax = 0.5f;   // 加速度上限
+
     private int _damage = 0;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        // 制御用パラメータ初期化
+        _preErrorPos = transform.position;   // Vector3D(x,y,z) -> Vector2D(x,y);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-
-        Debug.Log(_damage);
+        // ヘリを位置制御（ふわふわ飛んでいる感じを再現＆制御係数で感じの調整したい)
+        HeliController(Time.deltaTime, _heliTargetPos);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -23,8 +39,8 @@ public class EnemyMove : MonoBehaviour
         var bullet = other.gameObject.GetComponent<bulletMove>();
         if (bullet)
         {
-            _damage += bullet.GetBulletDamage();
-            Destroy(other.gameObject);
+            _damage += bullet.GetBulletDamage();    //  ダメージ加算
+            Destroy(other.gameObject);              //  弾丸消去
         }
         
 
@@ -36,7 +52,27 @@ public class EnemyMove : MonoBehaviour
         if (bullet)
         {
             _damage += bullet.GetBulletDamage();
+            Destroy(collision.gameObject);              //  弾丸消去
         }
  
+    }
+
+    private void HeliController(float delta_time, Transform targetPos)
+    {
+        // --- ヘリコプターのPID制御 --- //
+        // -- 誤差計算 -- //
+        Vector3 posError = targetPos.position - transform.position;
+        // -- 制御入力生成 -- //
+        _integralError = (posError + _preErrorPos) / 2 * delta_time;
+        Vector3 uf =    _kp * posError + 
+                        _ki * _integralError + 
+                        _kd * (posError - _preErrorPos) / delta_time;
+        // -- 制御入力をリジッドボディへ反映 -- //
+        Vector2 uf2D = uf;  // 3D -> 2D(x, y)落とし込み
+        _rb2D.AddForce(uf);
+
+        _preErrorPos = posError;    // 今フレームの誤差を格納しておく
+
+        // --- ヘリコプターの水平制御 --- //
     }
 }
